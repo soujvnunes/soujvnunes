49c0dab114fd273ac559234cb4835c103d733de5
Victor <soujvnunes@gmail.com>
2025-11-27 18:49:40 -0300

    chore: add global CSS styles using Linaria

--- ui/css.linaria.global.ts
+++ ui/css.linaria.global.ts
@@ -1,10 +1,8 @@
 import { css } from '@linaria/core'
-import theme from '../themizer.config'
+import { theme } from '../themizer.config'
 
 export const globals = css`
   :global() {
-    ${theme.rules.css}
-
     html,
     body {
       max-width: 100vw;
@@ -18,7 +16,7 @@ export const globals = css`
     body {
       color: ${theme.aliases.colors.ground.fore};
       background: ${theme.aliases.colors.ground.back};
-      font-family: var(--font-geist-sans), sans-serif;
+      font-family: ${theme.tokens.typeface.sans};
       font-size: ${theme.aliases.typography.body};
       -webkit-font-smoothing: antialiased;
       -moz-osx-font-smoothing: grayscale;
