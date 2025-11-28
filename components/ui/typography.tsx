0deb58dbee38b13804722b216eff1528bf321b6f
Victor <soujvnunes@gmail.com>
2025-11-28 10:37:23 -0300

    chore: add Headline and Subtitle components for typography

---
+++ components/ui/typography.tsx
@@ -0,0 +1,32 @@
+import { Slot } from '@radix-ui/react-slot'
+import { cn } from '@/lib/utils'
+
+export function Headline({
+  className,
+  asChild,
+  ...props
+}: React.ComponentProps<'h1'> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : 'h1'
+
+  return (
+    <Comp
+      className={cn('text-headline text-main text-balance', className)}
+      {...props}
+    />
+  )
+}
+
+export function Subtitle({
+  className,
+  asChild,
+  ...props
+}: React.ComponentProps<'p'> & { asChild?: boolean }) {
+  const Comp = asChild ? Slot : 'p'
+
+  return (
+    <Comp
+      className={cn('text-subtitle leading-[1.75] text-balance', className)}
+      {...props}
+    />
+  )
+}
