3df7e62a72b2745d9fbcf719f9fc262c6ef88d15
Victor <soujvnunes@gmail.com>
2025-11-28 10:37:48 -0300

    chore: remove Linaria styles and refactor Home component to use Headline and Subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,37 @@
 import Image from 'next/image'
-import {
-  Page,
-  Main,
-  Intro,
-  Ctas,
-  PrimaryLink,
-  SecondaryLink,
-  logo,
-} from './page.css'
+import { Headline, Subtitle } from '@/components/ui/typography'
 
 export default function Home() {
   return (
-    <Page>
-      <Main>
+    <>
+      <nav>
         <Image
-          className={logo}
           src="/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
           priority
         />
-        <Intro>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
+        <aside>socials</aside>
+      </nav>
+      <div role="feed">
+        <ul>
+          <li>
+            <article>hy</article>
+          </li>
+        </ul>
+      </div>
+      <main className="flex min-h-screen w-full flex-col items-start justify-between py-margin px-padding">
+        <div className="text-center">
+          <Headline>Lemme bring your ideas to life on the web!</Headline>
+          <Subtitle>
+            Explore my previous projects and get in touch to discuss how we can
+            collaborate.
+          </Subtitle>
+          <Subtitle>
             Looking for a starting point or more instructions? Head over to{' '}
             <a
+              className="font-medium text-main"
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer">
@@ -33,36 +39,38 @@ export default function Home() {
             </a>{' '}
             or the{' '}
             <a
+              className="font-medium text-main"
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer">
               Learning
             </a>{' '}
             center.
-          </p>
-        </Intro>
-        <Ctas>
-          <PrimaryLink
+          </Subtitle>
+        </div>
+        <div className="flex flex-row w-full max-w-[440px] gap-4">
+          <a
+            className="flex justify-center items-center h-10 px-4 rounded-full border border-transparent transition-ease cursor-pointer w-fit text-caption uppercase font-bold tracking-wide bg-ground-fore text-ground-back gap-2 hover:bg-main"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer">
             <Image
-              className={logo}
               src="/vercel.svg"
               alt="Vercel logomark"
               width={16}
               height={16}
             />
             Deploy Now
-          </PrimaryLink>
-          <SecondaryLink
+          </a>
+          <a
+            className="flex justify-center items-center h-10 px-4 rounded-full border border-transparent transition-ease cursor-pointer w-fit text-caption uppercase font-bold tracking-wide hover:opacity-80"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer">
             Documentation
-          </SecondaryLink>
-        </Ctas>
-      </Main>
-    </Page>
+          </a>
+        </div>
+      </main>
+    </>
   )
 }
