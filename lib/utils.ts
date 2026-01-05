2a5309a2acde13f986ea92fe88dc79b0dc20805a
Victor <soujvnunes@gmail.com>
2026-01-05 16:46:40 -0300

    chore: replace twMerge with extendedTailwindMerge for enhanced theme customization

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,14 @@
 import { clsx, type ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
+import { extendTailwindMerge } from 'tailwind-merge'
+
+const extendedTwMerge = extendTailwindMerge({
+  extend: {
+    theme: {
+      text: ['headline'],
+    },
+  },
+})
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return extendedTwMerge(clsx(inputs))
 }
