cf9e12403078ddcbf5ed8ffd08d790d5953cf92d
Victor <soujvnunes@gmail.com>
2026-01-20 12:13:50 -0300

    feat: remove deprecated authentication and data handling components

--- app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import { type Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "About",
-};
-
-export default function About() {
-  return <div>abouts</div>;
-}
--- components/Business/BusinessSocialsSkeleton.tsx
+++
@@ -1,9 +0,0 @@
-export default function BusinessSocialsSkeleton() {
-  return (
-    <aside className="space-x-sm flex animate-pulse" aria-hidden>
-      {[0, 1, 2, 3].map((bone) => (
-        <div className="Button ButtonMd bg-white/40" key={bone} />
-      ))}
-    </aside>
-  );
-}
