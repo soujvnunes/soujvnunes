3064c98dcdc3aae033928656e2a0a9a6faafaacf
Victor <soujvnunes@gmail.com>
2026-01-21 12:05:10 -0300

    feat: enhance opengraph image generation with dynamic font loading and improved layout

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,53 +1,75 @@
-import { ImageResponse } from "next/og";
+import { ImageResponse } from 'next/og'
 
-export const contentType = "image/png";
-export const alt = process.env.NEXT_PUBLIC_META_DESCRIPTION;
+import { readFile } from 'node:fs/promises'
+import { join } from 'node:path'
+
+export const contentType = 'image/png'
+export const alt = process.env.NEXT_PUBLIC_META_DESCRIPTION
 export const size = {
   width: 1200,
   height: 630,
-};
+}
 
-export default function Image() {
+export default async function opengraphImage() {
+  const sofiaProBold = await readFile(join(process.cwd(), 'app/sofia-pro/SofiaPro-Bold.otf'))
+
   return new ImageResponse(
-    (
-      <div
-        tw="flex flex-col justify-center px-10 w-full h-full text-[#FFFBF4]"
-        style={{
-          backgroundImage: "linear-gradient(to bottom, #1F1402, #100A01)",
-        }}
-      >
-        <strong tw="text-[64px]">
+    <div
+      tw="flex flex-col justify-center p-10 w-full h-full text-[#FFFBF4] items-end"
+      style={{
+        backgroundImage: 'linear-gradient(to bottom, #1F1402, #100A01)',
+      }}>
+      <h1 tw="text-[4rem] -mr-1 relative my-0">
+        <div tw="bottom-1 right-0 left-0 bg-[#d97706] absolute h-1 rounded-sm -z-1 mr-8" />
+        <span tw="z-1">
+          <span tw="text-[#FFFBF4]/40 -mr-4">https://</span>
           sou
           <span
             style={{
-              fontWeight: "medium",
-            }}
-          >
-            <span
-              style={{
-                letterSpacing: "-.2em",
-                color: "#6366F1",
-              }}
-            >
-              j
-            </span>
-            <span
-              style={{
-                color: "#F59E0B",
-              }}
-            >
-              v
-            </span>
+              letterSpacing: '-.2em',
+              color: '#6366F1',
+            }}>
+            j
           </span>
-          nunes
-        </strong>
-        <p tw="text-[40px] text-[#FFFBF4]/60">
-          Frontend Engineer and Product Designer
-        </p>
-      </div>
-    ),
+          <span
+            style={{
+              color: '#F59E0B',
+            }}>
+            v
+          </span>
+          nunes.github.io
+        </span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="40"
+          height="40"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#F59E0B"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          // @ts-expect-error Not implemented
+          tw="absolute -right-2 -bottom-4">
+          <path d="M14 4.1 12 6" />
+          <path d="m5.1 8-2.9-.8" />
+          <path d="m6 12-1.9 2" />
+          <path d="M7.2 2.2 8 5.1" />
+          <path d="M9.037 9.69a.498.498 0 0 1 .653-.653l11 4.5a.5.5 0 0 1-.074.949l-4.349 1.041a1 1 0 0 0-.74.739l-1.04 4.35a.5.5 0 0 1-.95.074z" />
+        </svg>
+      </h1>
+      <p tw="text-[2.5rem] text-[#FFFBF4]/60">Victor Nunes • Frontend Engineer</p>
+    </div>,
     {
       ...size,
+      fonts: [
+        {
+          name: 'Sofia Pro',
+          data: sofiaProBold,
+          style: 'normal',
+          weight: 700,
+        },
+      ],
     },
-  );
+  )
 }
