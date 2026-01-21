9e1379784501c6ed56b7fd2c4029e599f43fb9ab
Victor <soujvnunes@gmail.com>
2026-01-21 11:08:48 -0300

    feat: simplify sitemap function by returning a static route with last modified date

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,8 +1,8 @@
 export default function sitemap() {
-  const routes = ["", "/about"].map((route) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}${route}`,
-    lastModified: new Date().toISOString().split("T")[0],
-  }));
-
-  return routes;
+  return [
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+      lastModified: new Date().toISOString().split('T')[0],
+    },
+  ]
 }
