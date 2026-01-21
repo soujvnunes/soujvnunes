500cda0a7a962cefbf414ebb5403c875b9399032
Victor <soujvnunes@gmail.com>
2026-01-21 11:01:54 -0300

    feat: add Prettier configuration for code formatting

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('prettier').Config} */
-const config = {
+export default {
   jsxSingleQuote: false,
   bracketSameLine: true,
   singleAttributePerLine: true,
   singleQuote: true,
   semi: false,
   trailingComma: 'all',
+  enableDebugLogs: true,
   printWidth: 104,
-  objectWrap: 'collapse',
   tailwindStylesheet: './app/tailwind.config.css',
   plugins: ['prettier-plugin-tailwindcss'],
   tailwindFunctions: ['cva', 'twMerge', 'cn'],
+  bracketSpacing: true,
 }
-
-export default config
