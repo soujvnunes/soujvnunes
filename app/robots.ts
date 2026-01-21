82552eebf63a84394385e057ebb35494a6d749f1
Victor <soujvnunes@gmail.com>
2026-01-21 11:08:55 -0300

    fix: standardize quotes in robots function userAgent rule

--- app/robots.ts
+++ app/robots.ts
@@ -1,6 +1,6 @@
 export default function robots() {
   return {
-    rules: [{ userAgent: "*" }],
+    rules: [{ userAgent: '*' }],
     sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
-  };
+  }
 }
