578d316e32dcd1c98a0f998071a0d220b514aa45
Victor <soujvnunes@gmail.com>
2026-01-24 15:55:40 -0300

    refactor: enhance 404 page layout and styling for better user experience

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,12 @@
+import Link from 'next/link'
+
 export default function notFound() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        404 - Page Not Found
-      </h1>
-      <p className="mb-4">
-       The page you are looking for does not exist.
-      </p>
+    <section className="my-10 text-center">
+      <p className="text-caption font-semibold tracking-tighter uppercase">404</p>
+      <h1 className="text-headline leading-none font-bold text-balance">Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+      <Link href="/">Home</Link>
     </section>
   )
 }
