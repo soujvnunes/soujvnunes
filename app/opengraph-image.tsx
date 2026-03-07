83ba180d3c85cd963fed1e98feaf35b380fdc92c
Victor <soujvnunes@gmail.com>
2026-03-07 19:02:56 -0300

    refactor: correct spelling in opengraph image and adjust text opacity for consistency

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -23,34 +23,34 @@ export default async function opengraphImage() {
         <div tw="bottom-1 right-0 left-0 bg-[#d97706] absolute h-1 rounded-sm -z-1 mr-8" />
         <span tw="z-1">
           <span tw="text-[#FFFBF4]/40">https://</span>
-          souj
+          sou
           <span
             style={{
               letterSpacing: '-.2em',
               color: '#6366F1',
             }}>
-            v
+            j
           </span>
           <span
             style={{
               color: '#F59E0B',
             }}>
-            n
+            v
           </span>
-          un.es
+          nun.es
         </span>
         <svg
-          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
           width="40"
           height="40"
-          viewBox="0 0 24 24"
-          fill="none"
           stroke="#F59E0B"
+          viewBox="0 0 24 24"
           stroke-width="2"
           stroke-linecap="round"
           stroke-linejoin="round"
           // @ts-expect-error Not implemented
-          tw="absolute -right-2 -bottom-4">
+          tw="absolute -right-2 -bottom-4"
+          xmlns="http://www.w3.org/2000/svg">
           <path d="M14 4.1 12 6" />
           <path d="m5.1 8-2.9-.8" />
           <path d="m6 12-1.9 2" />
@@ -58,7 +58,7 @@ export default async function opengraphImage() {
           <path d="M9.037 9.69a.498.498 0 0 1 .653-.653l11 4.5a.5.5 0 0 1-.074.949l-4.349 1.041a1 1 0 0 0-.74.739l-1.04 4.35a.5.5 0 0 1-.95.074z" />
         </svg>
       </h1>
-      <p tw="text-[2.5rem] text-[#FFFBF4]/60">Victor Nunes • Senior Frontend Engineer</p>
+      <p tw="text-[2.5rem] text-[#FFFBF4]/40">Victor Nunes • Senior Frontend Engineer</p>
     </div>,
     {
       ...size,
