2497d066edeb9db14f363f6084af35881a3c1125
Victor <soujvnunes@gmail.com>
2026-03-09 17:48:57 -0300

    refactor: restructure RootPage component layout for improved semantics and styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import { HeroBackground } from '@/shared/components/background'
-import { Card } from '@/shared/components/card'
+import { Memoji } from '@/shared/components/Memoji'
+import { HeroBackground } from '@/shared/components/ui/background'
+import { Card } from '@/shared/components/ui/card'
 
 export default function RootPage() {
   return (
     <main className="flex min-h-dvh">
-      <Card className="m-auto">Victor Nunes</Card>
+      <Card className="m-auto grid w-3xs place-items-center">
+        <Memoji className="-mt-18" />
+        <h1 className="contents">
+          <span className="text-neutral-secondary text-caption font-bold tracking-wider uppercase">
+            Senior Frontend Engineer
+          </span>
+          <span className="text-body font-bold">Victor Nunes</span>
+        </h1>
+      </Card>
       <HeroBackground />
     </main>
   )
