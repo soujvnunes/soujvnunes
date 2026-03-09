fe96f8a03275d052e402b7e1e057be5b0deddd65
Victor <soujvnunes@gmail.com>
2026-03-09 17:35:57 -0300

    refactor: change Card component from div to article for semantic clarity and update class handling

--- shared/components/ui/card.tsx
+++ shared/components/ui/card.tsx
@@ -1,9 +1,9 @@
 import { cn } from '../../utils/shadcn'
 
 export const Card = ({ className, ...props }: React.ComponentProps<'div'>) => (
-  <div
+  <article
     className={cn(
-      'rounded-2xl bg-white p-2 shadow-lg shadow-amber-800/5 dark:border-t dark:border-t-amber-500/10 dark:bg-amber-900/20 dark:bg-linear-to-tl dark:from-amber-900/80 dark:shadow-none dark:backdrop-blur-2xl',
+      'rounded-2xl bg-white p-2 shadow-lg not-dark:shadow-amber-800/5 dark:border-t dark:border-t-amber-500/10 dark:bg-amber-1000/20 dark:bg-linear-to-tl dark:from-amber-1000/80 dark:to-indigo-950/40 dark:backdrop-blur-xs',
       className,
     )}
     {...props}
