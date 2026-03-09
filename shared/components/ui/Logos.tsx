d73dd66c0c9f3622f0a8d7c6bfde25aab22b8536
Victor <soujvnunes@gmail.com>
2026-03-09 18:23:54 -0300

    feat: add Logotype component for branding display

---
+++ shared/components/ui/Logos.tsx
@@ -0,0 +1,8 @@
+export const Logotype = () => (
+  <strong>
+    sou
+    <span className="tracking-[-0.2em] text-accent">j</span>
+    <span className="text-main">v</span>
+    nunes
+  </strong>
+)
