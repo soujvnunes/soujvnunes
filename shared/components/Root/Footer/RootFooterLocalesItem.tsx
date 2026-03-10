f225d73b0a0a06201ee9f344551b93c7359b86d1
Victor <soujvnunes@gmail.com>
2026-03-10 13:57:29 -0300

    feat: add RootFooterLocalesItem component for locale navigation and remove RootPresentationLocale component

---
+++ shared/components/Root/Footer/RootFooterLocalesItem.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+import { cn } from '@/shared/utils/shadcn'
+
+import type { I18nConfig } from '@/shared/lib/i18n'
+
+export const RootFooterLocalesItem = (props: Omit<I18nConfig, 'dict'>) => {
+  const pathname = usePathname()
+  const redirectedPathname = (locale: I18nConfig['locale']) => {
+    if (!pathname) return '/'
+
+    const segments = pathname.split('/')
+
+    segments[1] = locale
+
+    return segments.join('/')
+  }
+
+  return (
+    <Link
+      className={cn('text-caption font-bold', {
+        'text-neutral': pathname.includes(props.locale),
+      })}
+      href={redirectedPathname(props.locale)}>
+      {props.name}
+    </Link>
+  )
+}
