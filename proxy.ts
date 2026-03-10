739ac76b3d68f6fd31f6997970b67399622f43ff
Victor <soujvnunes@gmail.com>
2026-03-10 18:38:25 -0300

    fix: remove redundant static file checks in proxy function and update matcher

--- proxy.ts
+++ proxy.ts
@@ -6,35 +6,20 @@ import { getLocale, i18nConfig } from './shared/lib/i18n'
 export function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname
 
-  // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
-  // // If you have one
-  // if (
-  //   [
-  //     '/manifest.json',
-  //     '/favicon.ico',
-  //     // Your other files in `public`
-  //   ].includes(pathname)
-  // )
-  //   return
-
-  // Check if there is any supported locale in the pathname
-  const pathnameIsMissingLocale = i18nConfig.locales.every(
-    (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
-  )
+  if (/\.[a-zA-Z0-9]+$/.test(pathname)) return NextResponse.next() // Skip static files (anything with an extension like .pdf, .mov, .png, etc.)
 
   // Redirect if there is no locale
-  if (pathnameIsMissingLocale) {
-    const locale = getLocale(request)
-
-    // e.g. incoming request is /products
-    // The new URL is now /en-US/products
+  if (
+    i18nConfig.locales.every(
+      (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
+    ) // Check if there is any supported locale in the pathname
+  )
     return NextResponse.redirect(
-      new URL(`/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`, request.url),
-    )
-  }
+      new URL(`/${getLocale(request)}${pathname.startsWith('/') ? '' : '/'}${pathname}`, request.url),
+    ) // e.g. for incoming request is /products, the new URL is now /en-US/products
 }
 
 export const config = {
-  // Matcher ignoring `/_next/`, `/api/`, and static files (anything with an extension)
-  matcher: ['/((?!api|_next|[^/]*\\.[^/]*$).*)'],
+  // Matcher ignoring `/_next/` and `/api/`
+  matcher: ['/((?!api|_next).*)'],
 }
