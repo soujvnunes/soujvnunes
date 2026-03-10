6f1e09ebda677b60f38ee01eba3cc35961cbcca5
Victor <soujvnunes@gmail.com>
2026-03-10 11:24:43 -0300

    feat: update Tailwind theme to include 'main-secondary' text style

--- shared/utils/shadcn.ts
+++ shared/utils/shadcn.ts
@@ -4,7 +4,7 @@ import { extendTailwindMerge } from 'tailwind-merge'
 const extendedTwMerge = extendTailwindMerge({
   extend: {
     theme: {
-      text: ['headline'],
+      text: ['headline', 'main-secondary'],
     },
   },
 })
