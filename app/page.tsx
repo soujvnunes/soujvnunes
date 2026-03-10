328d09c029b16e3945390ce0b04f8884c87cc15f
Victor <soujvnunes@gmail.com>
2026-03-10 09:52:05 -0300

    feat: replace RootCard with RootPresentation in RootPage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import { RootCard } from '@/shared/components/Root/RootCard'
 import { RootFooter } from '@/shared/components/Root/RootFooter'
+import { RootPresentation } from '@/shared/components/Root/RootPresentation'
 import { HeroBackground } from '@/shared/components/ui/Background'
 
 export default function RootPage() {
   return (
     <>
       <main className="flex h-[calc(100dvh-40px)]">
-        <RootCard />
+        <RootPresentation />
       </main>
       <RootFooter />
       <HeroBackground />
