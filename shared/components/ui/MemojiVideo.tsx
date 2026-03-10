b0b635428d1c9b49996f023c36604575079490ae
Victor <soujvnunes@gmail.com>
2026-03-10 15:01:54 -0300

    feat: refactor MemojiVideo component to use useReducer for state management

--- shared/components/ui/MemojiVideo.tsx
+++ shared/components/ui/MemojiVideo.tsx
@@ -1,33 +1,74 @@
 'use client'
 
 import type { SuspenseProps } from 'react'
-import { useEffect, useState } from 'react'
+import { useReducer } from 'react'
 
 import { cn } from '@/shared/utils/shadcn'
+
+type State = {
+  canPlay: boolean
+  isPaused: boolean
+  isPlaying: boolean
+}
+type Action = { type: 'PAUSE' } | { type: 'PLAYING' } | { type: 'CAN_PLAY' } | { type: 'ENDED' }
+
+const initialState: State = {
+  isPlaying: false,
+  canPlay: false,
+  isPaused: false,
+}
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'PAUSE':
+      return { ...state, isPaused: true }
+    case 'PLAYING':
+      return { ...state, isPlaying: true, isPaused: false }
+    case 'CAN_PLAY':
+      return { ...state, canPlay: true }
+    case 'ENDED':
+      return { ...state, isPlaying: false }
+    default:
+      return state
+  }
+}
 
 export type MemojiVideoProps = React.ComponentProps<'video'> & SuspenseProps
 
 export const MemojiVideo = ({ fallback, className, ...props }: MemojiVideoProps) => {
-  const [supportsHevc, setSupportsHevc] = useState<boolean | null>(null)
-
-  useEffect(() => {
-    const video = document.createElement('video')
-    const canPlay = video.canPlayType('video/quicktime; codecs="hvc1"')
-
-    setSupportsHevc(canPlay === 'probably' || canPlay === 'maybe')
-  }, [])
-
-  if (!supportsHevc) return fallback
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const loading = (state.canPlay && state.isPaused) || !state.isPlaying
 
   return (
-    <video
-      muted
-      autoPlay
-      playsInline
-      preload="none"
-      crossOrigin="anonymous"
-      className={cn('h-full scale-150', className)}
-      {...props}
-    />
+    <>
+      <video
+        muted
+        autoPlay
+        playsInline
+        preload="none"
+        crossOrigin="anonymous"
+        onPause={() => dispatch({ type: 'PAUSE' })}
+        onPlaying={() => dispatch({ type: 'PLAYING' })}
+        onCanPlayThrough={() => dispatch({ type: 'CAN_PLAY' })}
+        onEnded={(event) => {
+          const video = event.currentTarget
+
+          video.currentTime = 7
+          video.play().catch(() => {
+            video.currentTime = 0
+
+            dispatch({ type: 'ENDED' })
+          })
+        }}
+        className={cn(
+          'h-full scale-150',
+          {
+            'pointer-events-none opacity-0': loading,
+          },
+          className,
+        )}
+        {...props}
+      />
+      {loading && fallback}
+    </>
   )
 }
