fdbd5d7ee36401ba3d910817c254bcdfedb05d7a
Victor <soujvnunes@gmail.com>
2026-07-22 11:52:18 -0300

    fix: remove Logotype component to simplify project structure

--- shared/components/ui/Logos.tsx
+++
@@ -1,8 +0,0 @@
-export const Logotype = () => (
-  <strong>
-    sou
-    <span className="tracking-[-0.2em] text-accent">j</span>
-    <span className="text-main">v</span>
-    nunes
-  </strong>
-)
