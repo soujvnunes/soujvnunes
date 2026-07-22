7dcd4e561df2e876169a51fbed5a0721be878a72
Victor <soujvnunes@gmail.com>
2026-07-22 11:52:23 -0300

    fix: remove Memoji component to simplify project structure

--- shared/components/ui/Memoji.tsx
+++
@@ -1,8 +0,0 @@
-import { cn } from '@/shared/utils/shadcn'
-
-export const Memoji = ({ className, ...props }: React.ComponentProps<'div'>) => (
-  <div
-    className={cn('inline-flex h-32 w-32 items-center justify-center overflow-hidden', className)}
-    {...props}
-  />
-)
